3f2a87951d9859e5101095a79d32569fa0f91e1d
YashwantSE <157872604+YashwantSE@users.noreply.github.com>
2025-07-27 14:04:36 +0530

    Add files via upload

---
+++ InterServices/consumer/src/main/java/com/yys/consumer/restTemplate/RestTemplateClient.java
@@ -0,0 +1,16 @@
+package com.yys.consumer.restTemplate;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@RequiredArgsConstructor
+public class RestTemplateClient {
+
+    private final RestTemplate restTemplate;
+    private static final String PROVIDER_URL = "http://localhost:8081";
+    public String getInstanceInfo(){
+      return restTemplate.getForObject(PROVIDER_URL + "/instance-info",String.class);
+    }
+}
---
+++ InterServices/consumer/src/test/java/com/yys/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yys.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
